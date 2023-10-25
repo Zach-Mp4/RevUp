5641a32309289669c80720a8a4698a4456ded9fc
Zachary Buchanan <zach@Zacharys-MBP.attlocal.net>
2023-10-25 17:58:29 -0500

    fixed navbar reload page bug

--- application/static/scripts/base.js
+++ application/static/scripts/base.js
@@ -1,20 +1,23 @@
 $(document).ready(function () {
-    $(window).on("resize", function() {
-        if (isNavbarTogglerVisible() && $('#user-nav').hasClass('navbar-right')) {
-          // Toggler button is visible
-          console.log("Navbar toggler button is visible.");
-          // Your code for the mobile view
-          $('#user-nav').removeClass("navbar-right");
-        } else if (!isNavbarTogglerVisible() && !$('#user-nav').hasClass('navbar-right')){
-          // Toggler button is hidden
-          console.log("Navbar toggler button is hidden.");
-          // Your code for the larger screen view
-          $('#user-nav').addClass("navbar-right");
-        }
-      });     
+    adjustElements()
+    $(window).on("resize", adjustElements);     
 });
 
 function isNavbarTogglerVisible() {
     let togglerButton = $(".navbar-toggler");
     return togglerButton.is(":visible");
   }
+
+function adjustElements(){
+    if (isNavbarTogglerVisible() && $('#user-nav').hasClass('navbar-right')) {
+        // Toggler button is visible
+        console.log("Navbar toggler button is visible.");
+        // Your code for the mobile view
+        $('#user-nav').removeClass("navbar-right");
+      } else if (!isNavbarTogglerVisible() && !$('#user-nav').hasClass('navbar-right')){
+        // Toggler button is hidden
+        console.log("Navbar toggler button is hidden.");
+        // Your code for the larger screen view
+        $('#user-nav').addClass("navbar-right");
+      }
+}
